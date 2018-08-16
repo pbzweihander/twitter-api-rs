e420c98b3487a40a50b0eb04a0d2beb7ca0e2e3f
Travis CI User <travis@example.org>
2018-08-16 06:48:58 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -85,6 +85,7 @@ ALIASES['tokio_tls'] = {};
 ALIASES['tokio_udp'] = {};
 ALIASES['try_lock'] = {};
 ALIASES['twitter_api'] = {};
+ALIASES['twitter_api'] = {};
 ALIASES['unicase'] = {};
 ALIASES['unicase'] = {};
 ALIASES['unicode_bidi'] = {};
