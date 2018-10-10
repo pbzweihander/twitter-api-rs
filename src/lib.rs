dec3288c98986084f937878c26a682cf60e80024
pbzweihander <sd852456@naver.com>
2018-10-10 13:40:41 +0900

    using my oauth-client branch to use hyper

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,11 @@ extern crate failure;
 extern crate oauth_client as oauth;
 #[macro_use]
 extern crate serde_derive;
+extern crate futures;
 extern crate serde_json;
 
+use futures::future::result;
+use futures::Future;
 use oauth::Token;
 use std::borrow::Cow;
 use std::collections::HashMap;
@@ -54,15 +57,20 @@ fn split_query<'a>(query: &'a str) -> HashMap<Cow<'a, str>, Cow<'a, str>> {
     param
 }
 
-pub fn get_request_token(consumer: &Token) -> Result<Token<'static>> {
-    let bytes = oauth::get(api_twitter_oauth::REQUEST_TOKEN, consumer, None, None)?;
-    let resp = String::from_utf8(bytes)?;
-    let param = split_query(&resp);
-    let token = Token::new(
-        param.get("oauth_token").unwrap().to_string(),
-        param.get("oauth_token_secret").unwrap().to_string(),
-    );
-    Ok(token)
+pub fn get_request_token(
+    consumer: &Token,
+) -> impl Future<Item = Token<'static>, Error = failure::Error> {
+    oauth::get(api_twitter_oauth::REQUEST_TOKEN, consumer, None, None)
+        .and_then(|bytes| result(String::from_utf8(bytes)).map_err(Into::into))
+        .map(|resp| {
+            let param = split_query(&resp);
+            let token = Token::new(
+                param.get("oauth_token").unwrap().to_string(),
+                param.get("oauth_token_secret").unwrap().to_string(),
+            );
+
+            token
+        })
 }
 
 pub fn get_authorize_url(request: &Token) -> String {
@@ -73,46 +81,56 @@ pub fn get_authorize_url(request: &Token) -> String {
     )
 }
 
-pub fn get_access_token(consumer: &Token, request: &Token, pin: &str) -> Result<Token<'static>> {
+pub fn get_access_token(
+    consumer: &Token,
+    request: &Token,
+    pin: &str,
+) -> impl Future<Item = Token<'static>, Error = failure::Error> {
     let mut param = HashMap::new();
     let _ = param.insert("oauth_verifier".into(), pin.into());
-    let bytes = oauth::get(
+    oauth::get(
         api_twitter_oauth::ACCESS_TOKEN,
         consumer,
         Some(request),
         Some(&param),
-    )?;
-    let resp = String::from_utf8(bytes)?;
-    let param = split_query(&resp);
-    let token = Token::new(
-        param.get("oauth_token").unwrap().to_string(),
-        param.get("oauth_token_secret").unwrap().to_string(),
-    );
-    Ok(token)
+    ).and_then(|bytes| result(String::from_utf8(bytes)).map_err(Into::into))
+    .map(|resp| {
+        let param = split_query(&resp);
+        let token = Token::new(
+            param.get("oauth_token").unwrap().to_string(),
+            param.get("oauth_token_secret").unwrap().to_string(),
+        );
+
+        token
+    })
 }
 
 /// function to update the status
 /// This function takes as arguments the consumer key, the access key, and the status (obviously)
-pub fn update_status(consumer: &Token, access: &Token, status: &str) -> Result<()> {
+pub fn update_status(
+    consumer: &Token,
+    access: &Token,
+    status: &str,
+) -> impl Future<Item = (), Error = failure::Error> {
     let mut param = HashMap::new();
     let _ = param.insert("status".into(), status.into());
-    let _ = oauth::post(
+    oauth::post(
         api_twitter_soft::UPDATE_STATUS,
         consumer,
         Some(access),
         Some(&param),
-    )?;
-    Ok(())
+    ).map(|_| ())
 }
 
-pub fn get_last_tweets(consumer: &Token, access: &Token) -> Result<Vec<Tweet>> {
-    let bytes = oauth::get(
+pub fn get_last_tweets(
+    consumer: &Token,
+    access: &Token,
+) -> impl Future<Item = Vec<Tweet>, Error = failure::Error> {
+    oauth::get(
         api_twitter_soft::HOME_TIMELINE,
         consumer,
         Some(access),
         None,
-    )?;
-    let last_tweets_json = String::from_utf8(bytes)?;
-    let ts = Tweet::parse_timeline(last_tweets_json)?;
-    Ok(ts)
+    ).and_then(|bytes| result(String::from_utf8(bytes)).map_err(Into::into))
+    .and_then(|last_tweets_json| result(Tweet::parse_timeline(last_tweets_json)))
 }
